e7fa3de27b7a5a2a9a3cceac5a7d35f295b20cc0
Natthanan Yaonopas <natthanan.y@360techx.co>
2026-04-17 09:49:39 +0700

    add allow local 4200 and add sawgger

--- Program.cs
+++ Program.cs
@@ -4,11 +4,23 @@ using queue_backend.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
     
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -40,8 +52,11 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseCors("LocalFrontend");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
